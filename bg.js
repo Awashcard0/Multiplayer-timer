3e8053ae0d6ab9b99cf2b12060543aa1199ae029
Awashcard0 <95319856+Awashcard0@users.noreply.github.com>
2022-10-20 09:47:21 -0400

    Delete bg.js

--- bg.js
+++
@@ -1,5 +0,0 @@
-let bg = document.querySelector(':root');
-let n = Math.floor(Math.random() * 26);
-if(!n) n = '';
-let result = `url("https://images.weserv.nl/?url=https://malwarewatch.org/images/backgrounds/background${n}.jpg")`;
-bg.style.setProperty('--result', result);
